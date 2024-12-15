5faa28d99604a024b37f9948898b40f817e24abd
milaanastasova <anastasova.mila@gmail.com>
2024-12-15 15:17:47 +0100

    updated AddButton.tsx

--- frontend/src/components/props/FormProps.ts
+++ frontend/src/components/props/FormProps.ts
@@ -1,13 +1,15 @@
 import { Category } from "../../models/Category";
 import { Account } from "../../models/Account";
+import { TransactionType } from "../../models/enum/TransactionType";
 
 export interface FormProps{
     name? : string;
     balance? : number;
     cost? : number;
     date? : Date;
+    type?: TransactionType;
     categories? : Category[];
     accounts? : Account[];
     pageName? : string;
-    onClose : () => void; 
+    onFormSubmitSuccess : () => void; 
 }
